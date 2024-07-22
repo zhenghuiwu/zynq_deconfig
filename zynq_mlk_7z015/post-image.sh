edc395c790df52c95f25a760f5e0aaa87e992c22
wu_zhenghui <wu_zhenghui>
2024-07-22 10:07:37 +0800

    change

--- zynq_mlk_7z015/post-image.sh
+++ zynq_mlk_7z015/post-image.sh
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #!/bin/sh
 
 # By default U-Boot loads DTB from a file named "devicetree.dtb", so
@@ -46,3 +47,20 @@ echo "go_to_ouput_dir=$OUTOUT_DIR"
 
 $BOOTGEN_DIR/bootgen -arch zynq -image system.bif -w on -o boot.bin
 
+=======
+#!/bin/sh
+
+# By default U-Boot loads DTB from a file named "devicetree.dtb", so
+# let's use a symlink with that name that points to the *first*
+# devicetree listed in the config.
+
+FIRST_DT=$(sed -n \
+           's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\([a-z0-9\-]*\).*"$/\1/p' \
+           ${BR2_CONFIG})
+
+[ -z "${FIRST_DT}" ] || ln -fs ${FIRST_DT}.dtb ${BINARIES_DIR}/system.dtb
+
+BOARD_DIR="$(dirname $0)"
+
+support/scripts/genimage.sh -c $BOARD_DIR/genimage.cfg
+>>>>>>> Stashed changes
